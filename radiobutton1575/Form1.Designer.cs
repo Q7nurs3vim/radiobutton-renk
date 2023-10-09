69d4f52f0007d6f55c9d3343cd2468e06b48b279
Luz <145450869+Q7nurs3vim@users.noreply.github.com>
2023-10-09 14:11:01 +0300

    Add files via upload

---
+++ radiobutton1575/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace radiobutton1575
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rbBlue = new System.Windows.Forms.RadioButton();
+            this.rbRed = new System.Windows.Forms.RadioButton();
+            this.rbBlack = new System.Windows.Forms.RadioButton();
+            this.rbWhite = new System.Windows.Forms.RadioButton();
+            this.btnDegistir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rbBlue
+            // 
+            this.rbBlue.AutoSize = true;
+            this.rbBlue.Location = new System.Drawing.Point(32, 59);
+            this.rbBlue.Name = "rbBlue";
+            this.rbBlue.Size = new System.Drawing.Size(48, 17);
+            this.rbBlue.TabIndex = 0;
+            this.rbBlue.TabStop = true;
+            this.rbBlue.Text = "Mavi";
+            this.rbBlue.UseVisualStyleBackColor = true;
+            // 
+            // rbRed
+            // 
+            this.rbRed.AutoSize = true;
+            this.rbRed.Location = new System.Drawing.Point(32, 97);
+            this.rbRed.Name = "rbRed";
+            this.rbRed.Size = new System.Drawing.Size(54, 17);
+            this.rbRed.TabIndex = 1;
+            this.rbRed.TabStop = true;
+            this.rbRed.Text = "Kırmızı";
+            this.rbRed.UseVisualStyleBackColor = true;
+            // 
+            // rbBlack
+            // 
+            this.rbBlack.AutoSize = true;
+            this.rbBlack.Location = new System.Drawing.Point(32, 135);
+            this.rbBlack.Name = "rbBlack";
+            this.rbBlack.Size = new System.Drawing.Size(51, 17);
+            this.rbBlack.TabIndex = 2;
+            this.rbBlack.TabStop = true;
+            this.rbBlack.Text = "Siyah";
+            this.rbBlack.UseVisualStyleBackColor = true;
+            // 
+            // rbWhite
+            // 
+            this.rbWhite.AutoSize = true;
+            this.rbWhite.Location = new System.Drawing.Point(32, 174);
+            this.rbWhite.Name = "rbWhite";
+            this.rbWhite.Size = new System.Drawing.Size(54, 17);
+            this.rbWhite.TabIndex = 3;
+            this.rbWhite.TabStop = true;
+            this.rbWhite.Text = "Beyaz";
+            this.rbWhite.UseVisualStyleBackColor = true;
+            // 
+            // btnDegistir
+            // 
+            this.btnDegistir.Location = new System.Drawing.Point(32, 222);
+            this.btnDegistir.Name = "btnDegistir";
+            this.btnDegistir.Size = new System.Drawing.Size(149, 23);
+            this.btnDegistir.TabIndex = 4;
+            this.btnDegistir.Text = "DEĞİŞTİR";
+            this.btnDegistir.UseVisualStyleBackColor = true;
+            this.btnDegistir.Click += new System.EventHandler(this.btnDegistir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(223, 294);
+            this.Controls.Add(this.btnDegistir);
+            this.Controls.Add(this.rbWhite);
+            this.Controls.Add(this.rbBlack);
+            this.Controls.Add(this.rbRed);
+            this.Controls.Add(this.rbBlue);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rbBlue;
+        private System.Windows.Forms.RadioButton rbRed;
+        private System.Windows.Forms.RadioButton rbBlack;
+        private System.Windows.Forms.RadioButton rbWhite;
+        private System.Windows.Forms.Button btnDegistir;
+    }
+}
+
---
+++ radiobutton1575/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace radiobutton1575
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDegistir_Click(object sender, EventArgs e)
+        {
+            if (rbBlue.Checked)
+            {
+               this.BackColor = Color.Blue;  
+            }
+            else if(rbRed.Checked) 
+            {
+              this.BackColor= Color.Red;
+            }
+            else if (rbWhite.Checked)
+            {
+               this.BackColor = Color.White;
+            }
+            else if (rbBlack.Checked)
+            {
+                this.BackColor= Color.Black;    
+            }
+        }  
+    }
+}
